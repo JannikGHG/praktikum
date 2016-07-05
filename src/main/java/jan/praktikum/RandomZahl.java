e6f488c4a2f5760720a9777f7b0988aec89b7b45
Praktikant <jannik.bensien@gmx.de>
2016-07-05 11:13:40 +0200

    Ausgabe einer Random Zahl

---
+++ src/main/java/jan/praktikum/RandomZahl.java
@@ -0,0 +1,11 @@
+package jan.praktikum;
+
+/**
+ * Created by Praktikant on 05.07.2016.
+ */
+public class RandomZahl {
+    public static void main(String... args) {
+        double number = Math.random();
+        System.out.println(number);
+    }
+}
