9692ada5250231b83c04f699b8cb7e417782bfd0
Praktikant <jannik.bensien@gmx.de>
2016-07-06 11:30:43 +0200

    Konversation mit Siri

--- src/main/java/jan/praktikum/LargeConversation.java
+++ src/main/java/jan/praktikum/LargeConversation.java
@@ -4,24 +4,26 @@ package jan.praktikum;
  * Created by Praktikant on 05.07.2016.
  */
 public class LargeConversation {
-    public static void main( String... args )
-    {
+    public static void main(String... args) {
         System.out.println("Hey wie heißt du?");
-        String name = new java.util.Scanner ( System.in ).nextLine();
+        String name = new java.util.Scanner(System.in).nextLine();
         System.out.println("Ah dein Name ist also " + name + " !");
-        System.out.println("Machst du gerade ein Praktikum?");
-        String praktikum = new java.util.Scanner ( System.in ).nextLine();
-        if (praktikum == "Ja") {
+        System.out.println("Ich heiße Siri");
+        System.out.println("Ich habe gehört du machst du gerade ein Praktikum?");
+        String praktikum = new java.util.Scanner(System.in).nextLine();
+        if ("Ja".equalsIgnoreCase(praktikum)) {
+            System.out.println("Wie viele Wochen dauert denn dein Praktikum?");
+            double dauerpraktikum = new java.util.Scanner(System.in).nextDouble();
+            double antwort = dauerpraktikum * 5;
+            System.out.println("Wow das sind ja " + antwort + " Tage!");
             System.out.println("Und bei welcher Firma?");
-            String firma = new java.util.Scanner ( System.in ).nextLine();
-            if (firma == "") {
+            String firma = new java.util.Scanner(System.in).nextLine();
+            if ("netcon7".equalsIgnoreCase(firma)) {
                 System.out.println("Ah die kenne ich doch die macht doch Software für Kraftwerke!");
-
-            }
-         else {
-                System.out.println("Na dann wünsche ich dir noch einen schönen Tag!");
-            }
-        }
-
+            } else
+                System.out.println("Oh die Firma kenne ich leider nicht");
+        } else
+            System.out.println("Oh dann habe ich mich wohl vertan.");
+            System.out.println("Ich muss weiter coden bin dann mal weg bye");
     }
 }
---
+++ src/main/java/jan/praktikum/StringComparison.java
@@ -0,0 +1,14 @@
+package jan.praktikum;
+
+/**
+ * Created by Praktikant on 06.07.2016.
+ */
+public class StringComparison {
+    public static void main (String... args) {
+        String praktikum = null;
+        if ("Ja".equals(praktikum)) {
+
+
+        }
+    }
+}
